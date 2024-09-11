7063834226d0b2318321930310b41aef1efd9081
Sahith53 <guthasahithchowdary@gmail.com>
2024-09-11 23:17:45 +0530

    hero section

---
+++ Front-end/src/components/LatestCollection.jsx
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+
+const LatestCollection = () => {
+console.log(sahit);
+// setSahit('moddagudu');
+
+console.log(products);
+
+  return (
+
+
+    // <div className='flex flex-wrap gap-2'> 
+    //  {products.map((i)=>{
+    //   return(
+    //     <div key={i.id}>
+    //       <h1 >{i.name}</h1>
+    //       <img src={i.image[0]} className=''/>
+    //       {/* <p>{i.description}</p> */}
+    //       <p>{i.price}</p>
+    //     </div>
+    //   )
+    //  })}
+
+    // </div>
+
+    <div className="">
+      
+    </div>
+  )
+}
+
+// for(int i =0; i < n; i++) {
+//   print(i);
+// }
+
+export default LatestCollection
---
+++ Front-end/src/context/ShopContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react";
+import { products } from "../assets/assets/frontend_assets/assets";
+
+export const ShopContext = createContext();
+
+const ShopContextProvider = (props) => {
+  const currency = "$";
+  const delivery_fee = 10;
+
+  const val = {
+    products,
+    currency,
+    delivery_fee,
+  };
+
+  return (
+    <ShopContext.Provider value={val}>{props.children}</ShopContext.Provider>
+  );
+};
+
+export default ShopContextProvider;
